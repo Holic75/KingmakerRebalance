65f87712805ab79eed4c3627c251540912e497e7
Holic75 <holic.denis@gmail.com>
2019-12-08 18:42:13 +0100

    added component for prerequisite alternative

---
+++ CallOfTheWild/NewMechanics/PrerequisiteMechanics.cs
@@ -0,0 +1,35 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Prerequisites;
+using Kingmaker.Blueprints.Classes.Selection;
+using Kingmaker.Blueprints.Facts;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Class.LevelUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallOfTheWild.PrerequisiteMechanics
+{
+
+    [AllowMultipleComponents]
+    public class PrerequsiteOrAlternative : Prerequisite
+    {        
+        public Prerequisite base_prerequsite, alternative_prerequsite;
+
+        public override bool Check(
+          FeatureSelectionState selectionState,
+          UnitDescriptor unit,
+          LevelUpState state)
+        {
+            return this.base_prerequsite.Check(selectionState, unit, state) || this.alternative_prerequsite.Check(selectionState, unit, state);
+        }
+
+        public override string GetUIText()
+        {
+            return base_prerequsite.GetUIText() + " or " + this.alternative_prerequsite.GetUIText();
+        }
+    }
+}
