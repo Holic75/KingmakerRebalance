467dfd5628e4ec616edd538dd049c9ab58db6822
Holic75 <holic.denis@gmail.com>
2020-08-22 16:36:30 +0200

    Update UnitViewMechanics.cs

--- CallOfTheWild/NewMechanics/UnitViewMechanics.cs
+++ CallOfTheWild/NewMechanics/UnitViewMechanics.cs
@@ -56,6 +56,7 @@ namespace CallOfTheWild.UnitViewMechanics
                 }
                 character.Start();
                 character.Animator.gameObject.AddComponent<UnitAnimationCallbackReceiver>();
+
                 character.Animator.cullingMode = AnimatorCullingMode.AlwaysAnimate;
                 if (character.AnimationManager)
                 {
