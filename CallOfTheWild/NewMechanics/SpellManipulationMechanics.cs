af3bf1cccca532026e65c0a59014fa73d0f6a0aa
Holic75 <holic.denis@gmail.com>
2020-10-29 12:50:39 +0100

    Update SpellManipulationMechanics.cs

--- CallOfTheWild/NewMechanics/SpellManipulationMechanics.cs
+++ CallOfTheWild/NewMechanics/SpellManipulationMechanics.cs
@@ -568,8 +568,8 @@ namespace CallOfTheWild
                         && __instance.MetamagicData != null
                         && __instance.MetamagicData.NotEmpty) 
                     {
-                        
-                        __result = (__instance.ConvertedFrom.Blueprint?.StickyTouch.TouchDeliveryAbility != __instance.Blueprint);//to avoid issues with touch spells that write their generating spells to ConvertedFrom
+
+                        __result = (__instance.ConvertedFrom.Blueprint?.StickyTouch?.TouchDeliveryAbility != __instance.Blueprint);//to avoid issues with touch spells that write their generating spells to ConvertedFrom
                     }
                 }
 
