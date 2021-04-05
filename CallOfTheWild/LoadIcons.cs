a47df3570761be931f199955d97abd4fc0a00268
Holic75 <holic.denis@gmail.com>
2021-04-05 18:01:35 +0200

    fixes
    - added pain descriptor to scourge attack buff
    - fixed new icon quality to be unaffected by texture settings

--- CallOfTheWild/LoadIcons.cs
+++ CallOfTheWild/LoadIcons.cs
@@ -18,7 +18,7 @@ namespace CallOfTheWild
             public static Sprite Create(string filePath)
             {
                 var bytes = File.ReadAllBytes(icons_folder + filePath);
-                var texture = new Texture2D(64, 64);
+                var texture = new Texture2D(64, 64, TextureFormat.DXT5, false);
                 texture.LoadImage(bytes);
                 return Sprite.Create(texture, new Rect(0, 0, 64, 64), new Vector2(0, 0));
             }
