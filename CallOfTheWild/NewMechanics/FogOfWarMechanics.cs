1b497b8ad66191051758b394e27a4f72529df469
Holic75 <holic.denis@gmail.com>
2021-05-02 22:42:56 +0200

    Update FogOfWarMechanics.cs
    - prevented arcane eye from interacting with objects on map

--- CallOfTheWild/NewMechanics/FogOfWarMechanics.cs
+++ CallOfTheWild/NewMechanics/FogOfWarMechanics.cs
@@ -3,7 +3,9 @@ using Kingmaker;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Facts;
 using Kingmaker.Controllers;
+using Kingmaker.Controllers.Clicks.Handlers;
 using Kingmaker.EntitySystem.Entities;
+using Kingmaker.UI.Selection;
 using Kingmaker.UnitLogic;
 using Kingmaker.Utility;
 using System;
@@ -11,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace CallOfTheWild.FogOfWarMechanics
 {
@@ -74,6 +77,23 @@ namespace CallOfTheWild.FogOfWarMechanics
         static void Postfix(UnitEntityData __instance, ref bool __result)
         {
             __result = __result || (__instance?.Get<UnitPartFogOfWarRevealer>()?.active()).GetValueOrDefault();
+        }
+    }
+
+
+
+    [Harmony12.HarmonyPatch(typeof(SelectionManager))]
+    [Harmony12.HarmonyPatch("GetNearestSelectedUnit", Harmony12.MethodType.Normal)]
+    public class SelectionManager_GetNearestSelectedUnit_Patch
+    {
+        static bool Prefix(SelectionManager __instance, Vector3 point, ref UnitEntityData __result)
+        {
+            if (__instance.SelectedUnits.Count > 1)
+                __result = __instance.SelectedUnits.Where<UnitEntityData>((Func<UnitEntityData, bool>)(u =>!(u.Get<UnitPartFogOfWarRevealer>()?.active()).GetValueOrDefault())).OrderBy<UnitEntityData, float>((Func<UnitEntityData, float>)(u => u.DistanceTo(point))).FirstOrDefault<UnitEntityData>();
+            else
+                __result = __instance.SelectedUnits.FirstOrDefault<UnitEntityData>(u => !(u.Get<UnitPartFogOfWarRevealer>()?.active()).GetValueOrDefault());
+
+            return false;
         }
     }
 
